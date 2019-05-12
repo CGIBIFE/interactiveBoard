8ec39df6020492fcd90db16e6359db66ec340b71
CGIBIFE <arul.natarajan@cgi.com>
2019-05-12 22:07:06 +0200

    use local socket

--- src/service/socket.client.ts
+++ src/service/socket.client.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 export class SocketService {
     private socket;
     constructor() {
-        this.socket = io('https://socker-server.herokuapp.com/', {transports: ['websocket']});
+        this.socket = io('ws://localhost', {transports: ['websocket']});
     }
 
     public createRoom(name) {
